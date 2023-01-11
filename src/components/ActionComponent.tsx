ed97e7f561c9c963b078e12d15e29862e71dcdcc
Tom Macfie <tm@paceteq.com>
2023-01-11 14:39:26 +0100

    feat: actions for arrays
    Signed-off-by: Tom Macfie <tm@paceteq.com>

--- src/components/ActionComponent.tsx
+++ src/components/ActionComponent.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
 import { ActionEnum, useMyContext } from '../hooks/context';
 
 const ActionComponent = () => {
   const { state, dispatch } = useMyContext();
   return (
     <>
-      <button
-        onClick={() => {
-          dispatch(ActionEnum.INCREASE);
-        }}
-      >
-        Increment
-      </button>
-      <button
-        onClick={() => {
-          dispatch(ActionEnum.DECREASE);
-        }}
-      >
-        Decrement
-      </button>
-      <div>Value: {state}</div>
+      <div>
+        <div>Numbers</div>
+        <button
+          onClick={() => {
+            dispatch({ type: ActionEnum.INCREASE });
+          }}
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => {
+            dispatch({ type: ActionEnum.DECREASE });
+          }}
+        >
+          Decrement
+        </button>
+        <div>Value: {state.number}</div>
+      </div>
+      <div>
+        <div>Arrays</div>
+        <input
+          type="text"
+          onChange={(event) => {
+            dispatch({
+              type: ActionEnum.ADDTOARRAY,
+              payload: event.target.value,
+            });
+          }}
+        />
+        <ul>
+          {state.array.map((item, index) => {
+            return (
+              <div style={{ display: 'flex' }}>
+                <li key={item}>{item}</li>
+                <button
+                  onClick={() => {
+                    dispatch({
+                      type: ActionEnum.REMOVEFROMARRAY,
+                      payload: index,
+                    });
+                  }}
+                >
+                  Remove from list
+                </button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
--- src/hooks/context.tsx
+++ src/hooks/context.tsx
@@ -5,18 +5,50 @@ import React, {
   useReducer,
 } from 'react';
 
+export type StateType = {
+  number: number;
+  array: string[];
+};
+
 export type MyContextType = {
-  state: number;
-  dispatch: React.Dispatch<ActionEnum>;
+  state: StateType;
+  dispatch: React.Dispatch<
+    IncreaseActionType | DecreaseActionType | AddToArray | RemoveFromArray
+  >;
 };
 
 export enum ActionEnum {
   INCREASE = 'INCREASE',
   DECREASE = 'DECREASE',
+  ADDTOARRAY = 'ADDTOARRAY',
+  REMOVEFROMARRAY = 'REMOVEFROMARRAY',
 }
 
+export type IncreaseActionType = {
+  type: ActionEnum.INCREASE;
+};
+export type DecreaseActionType = {
+  type: ActionEnum.DECREASE;
+};
+export type AddToArray = {
+  type: ActionEnum.ADDTOARRAY;
+  payload: string;
+};
+export type RemoveFromArray = {
+  type: ActionEnum.REMOVEFROMARRAY;
+  payload: number;
+};
+
+const initialState = {
+  number: 0,
+  array: [],
+};
+
 const MyContext = createContext<MyContextType>({
-  state: 0,
+  state: {
+    number: 0,
+    array: [],
+  },
   dispatch: () => {},
 });
 
@@ -25,22 +57,41 @@ export const MyContextProvider = ({
 }: {
   children: ReactElement[];
 }) => {
-  const myReducer = (state: number, action: ActionEnum) => {
-    let newState;
-    switch (action) {
-      case ActionEnum.INCREASE:
-        newState = state + 1;
+  const myReducer = (
+    state: StateType,
+    action:
+      | IncreaseActionType
+      | DecreaseActionType
+      | AddToArray
+      | RemoveFromArray
+  ) => {
+    let newState: StateType;
+    switch (action.type) {
+      case 'INCREASE':
+        newState = { ...state, number: state.number + 1 };
         break;
-      case ActionEnum.DECREASE:
-        newState = state - 1;
+      case 'DECREASE':
+        newState = { ...state, number: state.number - 1 };
         break;
+      case 'ADDTOARRAY':
+        newState = { ...state, array: [...state.array, action.payload] };
+        break;
+      case 'REMOVEFROMARRAY': {
+        const newArray = [...state.array];
+        newArray.splice(action.payload, 1);
+        newState = {
+          ...state,
+          array: newArray,
+        };
+        break;
+      }
       default:
         newState = state;
     }
     return newState;
   };
 
-  const [state, dispatch] = useReducer(myReducer, 0);
+  const [state, dispatch] = useReducer(myReducer, initialState);
 
   return (
     <MyContext.Provider value={{ state, dispatch }}>
